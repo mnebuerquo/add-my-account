44adfe66acfea2c5289a3ca77fc735105da2c1e4
mnebuerquo <sherman.adelson@gmail.com>
2016-01-28 14:11:03 +0000

    detecting user account if it exists

--- add-my-account.sh
+++ add-my-account.sh
@@ -6,5 +6,11 @@ if [ $(id -u) -ne 0 ]; then
 	exit 1;
 fi
 
+USERNAME=sherman
 
+# next determine if user exists
+# create user if not
+NEED_USER=$(id -u $USERNAME > /dev/null 2>&1; echo $?)
+
+echo $NEED_USER;
 
