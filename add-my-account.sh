b09b95c10271132b9f035ac40e87c9cc03b4f84f
mnebuerquo <sherman.adelson@gmail.com>
2016-01-28 18:41:32 +0000

    adds user and sets password now on ubuntu 14

--- add-my-account.sh
+++ add-my-account.sh
@@ -6,11 +6,37 @@ if [ $(id -u) -ne 0 ]; then
 	exit 1;
 fi
 
-USERNAME=sherman
+USERNAME=testuser
+
+# generate random password
+# generate password in advance in case we need it
+# http://serverfault.com/a/261417/33170
+PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1);
 
 # next determine if user exists
-# create user if not
 NEED_USER=$(id -u $USERNAME > /dev/null 2>&1; echo $?)
+if [ $NEED_USER -ne 0 ]; then
 
-echo $NEED_USER;
+	# create user if not
+	echo "Creating user: $USERNAME"
+	adduser --disabled-password --gecos "" $USERNAME
+
+	# change password programmatically
+	# this isn't secure against other logged in users who can use w or ps
+	# using passwd didn't work for me on ubuntu 14, so use chpasswd instead
+	# http://www.systutorials.com/39549/changing-linux-users-password-in-one-command-line/
+	# http://unix.stackexchange.com/a/57804/85863
+	echo "Setting password for $USERNAME."
+	echo "$USERNAME:$PASSWORD" | chpasswd
+	echo
+	echo "User password set to $PASSWORD so make sure to text it to $USERNAME."
+	echo
+
+else
+	echo "User $USERNAME already exists here."
+fi
+
+# make sure new user is in sudo group
+echo "Adding user to sudo: $USERNAME"
+adduser $USERNAME sudo
 
